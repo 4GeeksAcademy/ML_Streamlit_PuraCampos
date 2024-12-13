0507ac5cf35a097a41ae080c2514d74f4703e30c
PuraCampos <camposllinares@gmail.com>
2024-12-13 18:19:10 +0000

    13/12/2024

--- src/app.py
+++ src/app.py
@@ -1,9 +1,16 @@
-# from utils import db_connect
-# engine = db_connect()
-
 import streamlit as st
-# from pickle import load
-# import numpy as np
-# from gtts import gTTS
+import pandas as pd
+import numpy as np
+from PIL import Image
+from pickle import load
 
-st.title("Hello, World!")
+model=load(open('./models/decision_tree_classifier_default_42.sav','rb'))
+
+st.set_page_config(layout="wide")
+st.write("# My app *Margarita*")
+file=st.file_uploader("Pick a file", accept_multiple_files=True)
+
+st.title("Your details")
+# st.write(pd.read_csv(''))
+
+
