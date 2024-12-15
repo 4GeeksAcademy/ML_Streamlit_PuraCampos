4f34177e2033f9c167eb2cf65ff5530a70da5185
PuraCampos <camposllinares@gmail.com>
2024-12-15 19:38:21 +0000

    15/12/2024

--- src/app.py
+++ src/app.py
@@ -8,6 +8,7 @@ from pickle import load
 model=load(open('./models/decision_tree_classifier_default_42.sav','rb'))
 
 idioma = {"Español": "es"}
+outcome = {"0": "iris setosa", "1": "iris versicolor","2": "iris virginica"}
 
 with st.sidebar:
     st.title("Lirio tuberoso")
@@ -25,6 +26,11 @@ text_labels = {
     "sepal width (cm)": "sepal width (cm)",
     "petal length (cm)": "petal length (cm)",
     "petal width (cm)": "petal width (cm)",
+    "Resultado": "Resultado",
+    "iris setosa": "iris setosa",
+    "iris versicolor": "iris versicolor",
+    "iris virginica": "iris virginica",
+
 }
 st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Iris_tuberosa_Ypey72.jpg/250px-Iris_tuberosa_Ypey72.jpg", width=100)
 st.title(text_labels["title"])
@@ -33,8 +39,31 @@ st.markdown(text_labels["instructions"])
 
 left, right = st.columns(2)
 with left:
-    sepal_length_cm = st.text_input(text_labels["sepal length (cm)"])
-    sepal_width_cm = st.text_input(text_labels["sepal width (cm)"])
+    sepal_length_cm = st.number_input(text_labels["sepal length (cm)"])
+    sepal_width_cm = st.number_input(text_labels["sepal width (cm)"])
 with right:
-    petal_length_cm = st.text_input(text_labels["petal length (cm)"])
-    petal_width_cm = st.text_input(text_labels["petal width (cm)"])
+    petal_length_cm = st.number_input(text_labels["petal length (cm)"])
+    petal_width_cm = st.number_input(text_labels["petal width (cm)"])
+
+if st.button(text_labels["Resultado"]):
+    if not sepal_length_cm or not sepal_width_cm or not petal_length_cm or not petal_width_cm :
+        st.error("Por favor, complete todos los campos antes de continuar.")
+    else:
+        sepal_length_cm = float(sepal_length_cm)
+        sepal_width_cm = float(sepal_width_cm)
+        petal_length_cm = float(petal_length_cm)
+        petal_width_cm = float(petal_width_cm)
+
+
+        data = np.array([[5.1, 3.5, 1.4, 0.2]])
+        st.info(text_labels["success"])
+        prediction = str(model.predict(data)[0])
+        if prediction == "0":
+            result = text_labels["iris setosa"]
+        elif prediction == "1":
+            result = text_labels["iris versicolor"]
+        else:
+            result = text_labels["iris virginica"]
+
+        st.success(result)
+        st.set_option
